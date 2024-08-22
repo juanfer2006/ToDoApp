a6afef38bb3dd4287a3eda5027955a8dbdda6f48
phpg21 <ppalaciosgoez@gmail.com>
2024-08-22 11:25:00 -0500

    add class TodoBook

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -13,7 +13,15 @@ class Todo:
 
     def add_tag(self, tag: str):
         if tag not in self.tags:
-            self.tags.append()
+            self.tags.append(tag)
 
     def __str__(self):
         return f"{self.code_id}-{self.title}"
+
+
+class TodoBook:
+
+    def __int__(self, todos):
+        self.todos: dict[int, todos] = {}
+
+    def add_todo
