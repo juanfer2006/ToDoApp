4ca570e878ba2f8d65e51238f327187324f30871
phpg21 <ppalaciosgoez@gmail.com>
2024-08-22 10:40:05 -0500

    add class Todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,9 @@
 # TODO: Add code here
+class Todo:
+
+    def __int__(self, code_id: int, title: str, description: str, completed: bool, tags: list[str]):
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: False = completed
+        self.tags: list[str] = tags
